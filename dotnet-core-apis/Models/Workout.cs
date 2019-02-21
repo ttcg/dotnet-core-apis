88931bec8f3ae30b24263c4a4024ec78c0a93cc1
Thet Wai <thetwai2000@gmail.com>
2019-02-21 22:03:56 +0000

    changed the name of the property  from WorkoutDate to date

--- dotnet-core-apis/Models/Workout.cs
+++ dotnet-core-apis/Models/Workout.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace dotnet_core_apis.Models
 {
@@ -6,6 +7,7 @@ namespace dotnet_core_apis.Models
     {
         public Guid Id { get; set; }
         public string WorkoutType { get; set; }
+        [JsonProperty(PropertyName="date")]
         public DateTime WorkoutDate { get; set; }
         public int Calories { get; set; }
     }
